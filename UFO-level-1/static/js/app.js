c138d0f846b4578b4c772867fc21f8135a134c5f
Jeremy Hamley <jeremyhamley@gmail.com>
2020-08-21 19:35:29 -0400

    update Java

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,11 @@
 // from data.js
 var tableData = data;
 
-// 
+// check all data
 console.log(tableData);
 
-var tbody = d3.select("tbody");
-var inputField = d3.select("#datetime");
-var button = d3.select("#filter-btn");
 
-
+// load the table with all of the sightings data
 tableData.forEach((sighting) => {
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
@@ -17,7 +14,12 @@ tableData.forEach((sighting) => {
     });
   });
 
-  
+// add var's for SELECT
+var tbody = d3.select("tbody");
+var inputField = d3.select("#datetime");
+var button = d3.select("#filter-btn");
+
+
 
 
 
