71d4331a0cfdc849b99fc3c0883c95134129351e
Jeremy Hamley <jeremyhamley@gmail.com>
2020-08-21 21:58:44 -0400

    save JavaScript

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,8 +6,9 @@ console.log(tableData);
 
 // add var's for SELECT
 var tbody = d3.select("tbody");
-var form = d3.select(".filter list-group-item");
-var button = d3.select("#filter-btn");
+var form = d3.select("form");
+var filter_button = d3.select("#filter-btn");
+var clear_button = d3.select("#clear-btn");
 
 
 // load the table with all of the sightings data
@@ -19,15 +20,33 @@ tableData.forEach((sighting) => {
     });
   });
 
+
+
 // Create event handlers 
-form.on("submit",runEnter);
-button.on("click", runEnter);
+form.on("submit", runFilter);
+filter_button.on("click", runFilter);
+clear_button.on("click", runClear);
 
-// define runEnter function
-function runEnter() {
+// define clear filter function
+function runClear() {
+    d3.event.preventDefault();
+    // clear the table
+    tbody.html("");
+    tableData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+}
 
-    // // Prevent the page from refreshing
-    // d3.event.preventDefault();
+
+// define runEnter filter function
+function runFilter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
     
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
