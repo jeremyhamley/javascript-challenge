be5e115fe3a14fa26fd8dfe51a20ae6e37de08a4
Jeremy Hamley <jeremyhamley@gmail.com>
2020-08-21 20:40:30 -0400

    save progress

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,6 +4,11 @@ var tableData = data;
 // check all data
 console.log(tableData);
 
+// add var's for SELECT
+var tbody = d3.select("tbody");
+var form = d3.select(".filter list-group-item");
+var button = d3.select("#filter-btn");
+
 
 // load the table with all of the sightings data
 tableData.forEach((sighting) => {
@@ -14,10 +19,41 @@ tableData.forEach((sighting) => {
     });
   });
 
-// add var's for SELECT
-var tbody = d3.select("tbody");
-var inputField = d3.select("#datetime");
-var button = d3.select("#filter-btn");
+// Create event handlers 
+form.on("submit",runEnter);
+button.on("click", runEnter);
+
+// define runEnter function
+function runEnter() {
+
+    // // Prevent the page from refreshing
+    // d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+  
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    // clear the table
+    tbody.html("");
+
+    filteredData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+
+}
 
 
 
