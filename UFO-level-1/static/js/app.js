e735928ef3122db7795d87aff54029ac4c65fc69
Jeremy Hamley <jeremyhamley@gmail.com>
2020-08-22 16:06:21 -0400

    Sava Java

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,6 +10,8 @@ var clear_button = d3.select("#clear-btn");
 var date_form = d3.select("#date_form");
 var city_form = d3.select("#city_form");
 var state_form = d3.select("#state_form");
+var country_form = d3.select("#country_form");
+var shape_form = d3.select("#shape_form");
 
 
 
@@ -29,6 +31,8 @@ clear_button.on("click", runClear);
 date_form.on("submit", runFilterDate);
 city_form.on("submit", runFilterCity);
 state_form.on("submit", runFilterState);
+country_form.on("submit", runFilterCountry);
+shape_form.on("submit", runFilterShape);
 
 
 // define runClear function
@@ -73,6 +77,7 @@ function runFilterDate() {
       });
 }
 
+
 // define runFilterCity function
 function runFilterCity() {
   // Prevent the page from refreshing
@@ -101,6 +106,88 @@ function runFilterCity() {
 }
 
 
+// define runFilterState function
+function runFilterState() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#state");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  // filter by city based on input value
+  var filteredData = tableData.filter(ufo => ufo.state === inputValue);
+  console.log(filteredData);
+
+  // clear the table
+  tbody.html("");
+
+  //populate table with filered data
+  filteredData.forEach((sighting) => {
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+}
+
+
+// define runFilterCountry function
+function runFilterCountry() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#country");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  // filter by city based on input value
+  var filteredData = tableData.filter(ufo => ufo.country === inputValue);
+  console.log(filteredData);
+
+  // clear the table
+  tbody.html("");
+
+  //populate table with filered data
+  filteredData.forEach((sighting) => {
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+}
+
+
+// define runFilterShape function
+function runFilterShape() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#shape");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  // filter by city based on input value
+  var filteredData = tableData.filter(ufo => ufo.shape === inputValue);
+  console.log(filteredData);
+
+  // clear the table
+  tbody.html("");
+
+  //populate table with filered data
+  filteredData.forEach((sighting) => {
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+}
 
 
 
